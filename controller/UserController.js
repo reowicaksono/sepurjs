54eba2163d564e27e10a117bf2b8e84a3db01f29
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-01-04 10:12:14 +0700

    add logic user

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,3 +1,4 @@
+import bycript from "bcrypt";
 import User from "../models/UserModels.js";
 
 
@@ -8,4 +9,20 @@ export const getAllUsers = async (req, res) => {
     } catch (error) {
         res.json({ message: error });
     }
+};
+
+export const createUser = async (req,res) => {
+    const {name,email,password, confirmPassword} = req.body;
+    if(password !== confirmPassword) return res.status(400).json({message: "Password not match"});
+    const encrypt = await bycript.genSalt();
+    const passwordHash = await bycript.hash(password, encrypt);
+     try {
+       const user = await User.create({name: name, email: email, password: passwordHash});
+       res.json(user, {message: `User Created ${name}`});
+
+    }catch (error) {
+        res.json({ message: error });
+    }
+        
+
 };
