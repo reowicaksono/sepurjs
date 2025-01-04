470eed07bef87b485127eb5f6f4dbd2095572eb6
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-01-04 09:50:42 +0700

    init db

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from 'express';
+import db from './config/Database.js';
 
 const app = express();
+
+try{
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+}catch (error){
+    console.log('Unable to connect to the database:', error);
+}
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
