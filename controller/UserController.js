b59036b8a733f96683de6b1e5ff10e6dcfac88fd
reowicaksono <reo.wicaksono.2005356@gmail.com>
2025-01-04 10:04:37 +0700

    fixing error file path

---
+++ controller/UserController.js
@@ -0,0 +1,11 @@
+import User from "../models/UserModels.js";
+
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.findAll();
+        res.json(users);
+    } catch (error) {
+        res.json({ message: error });
+    }
+};
--- routes/app_routes.js
+++ routes/app_routes.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { getAllUsers } from "../controller/UserController.js";
 
 const appRouter = express.Router();
 
-appRouter.get("/", (req, res) => {
-    res.send("Hello World");
-})
+appRouter.get("/user",getAllUsers);
 
 export default appRouter;
